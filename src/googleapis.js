a4433ab276e690c1e4492acb066791efab148447
Sho Kusano <rosylilly@aduca.org>
2020-02-27 16:55:37 +0900

    Support multi page folder

--- src/googleapis.js
+++ src/googleapis.js
@@ -1,6 +1,8 @@
 const { GoogleToken } = require('gtoken');
 const request = require('request');
 
+const PageSize = 100;
+
 const getToken = (key, email) => {
   return new Promise((resolve, reject) => {
     const gtoken = new GoogleToken({
@@ -29,18 +31,58 @@ const getFolder = (folderId, token) => {
         },
         qs: {
           q: `'${folderId}' in parents`,
+          orderBy: 'name',
+          pageSize: PageSize,
         },
       },
       (err, res, body) => {
         if (err) {
           reject(err);
         } else {
-          resolve(JSON.parse(body).files);
+          const parsed = JSON.parse(body);
+          if (parsed.nextPageToken && parsed.files.length === PageSize) {
+            getFolderNextPage(folderId, token, parsed.nextPageToken, (files) => {
+              resolve(parsed.files.concat(files));
+            }, reject);
+          } else {
+            resolve(parsed.files);
+          }
         }
       }
     );
   });
 };
+
+const getFolderNextPage = (folderId, token, pageToken, resolve, reject) => {
+  request(
+    {
+      uri: `https://www.googleapis.com/drive/v3/files`,
+      auth: {
+        bearer: token,
+      },
+      qs: {
+        q: `'${folderId}' in parents`,
+        orderBy: 'name',
+        pageSize: PageSize,
+        pageToken: pageToken,
+      },
+    },
+    (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        const parsed = JSON.parse(body);
+        if (parsed.nextPageToken && parsed.files.length === PageSize) {
+          getFolderNextPage(folderId, token, parsed.nextPageToken, (files) => {
+            resolve(parsed.files.concat(files));
+          }, reject);
+        } else {
+          resolve(parsed.files);
+        }
+      }
+    }
+  )
+}
 
 const getFile = (fileId, token) => {
   return new Promise((resolve, reject) => {
